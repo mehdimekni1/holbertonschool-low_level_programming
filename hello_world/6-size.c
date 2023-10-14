c6a897e475690ffd07a6ffe3afce07a9dae47ce0
mehdimekni1 <7712@holbertonstudents.com>
2023-10-14 04:40:34 -0700

    print f size

---
+++ hello_world/6-size.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ *
+ * Return: 0
+ */
+int main(void)
+{
+printf("Size of char: %zu bytes\n", sizeof(charType));
+printf("Size of int: %zu bytes\n", sizeof(intType));
+printf("Size of longint: %zu bytes\n", sizeof(longintType));
+printf("Size of longlongint: %zu bytes\n", sizeof(longlongintType));
+printf("Size of float: %zu bytes\n", sizeof(floatType));
+	
+return (0);
+}
