7efa63f2fe178e78380e04b9a6e280b4e8b94651
mehdimekni1 <7712@holbertonstudents.com>
2023-10-14 04:44:58 -0700

    print f size

--- hello_world/6-size.c
+++ hello_world/6-size.c
@@ -7,10 +7,10 @@
  */
 int main(void)
 {
-printf("Size of char: %zu bytes\n", sizeof(charType));
-printf("Size of int: %zu bytes\n", sizeof(intType));
-printf("Size of longint: %zu bytes\n", sizeof(longintType));
-printf("Size of longlongint: %zu bytes\n", sizeof(longlongintType));
+printf("Size of a char: %zu bytes\n", sizeof(charType));
+printf("Size of an int: %zu bytes\n", sizeof(intType));
+printf("Size of a long int: %zu bytes\n", sizeof(longintType));
+printf("Size of a long long int: %zu bytes\n", sizeof(llgType));
 printf("Size of float: %zu bytes\n", sizeof(floatType));
 	
 return (0);
