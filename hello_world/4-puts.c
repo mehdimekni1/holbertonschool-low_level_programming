2cc059e69ec9014ea296a87a18ab593dbe02c1e5
mehdimekni1 <7712@holbertonstudents.com>
2023-10-14 02:45:51 -0700

    puts c

--- hello_world/4-puts.c
+++ hello_world/4-puts.c
@@ -1,6 +1,13 @@
 #!/bin/bash
 #include <stdio.h>
-int main() {
-	puts ("\"Programming is like building a multilingual puzzle")
-return 0;
+
+/**
+ * main - Entry point
+ * 
+ * Return: 0
+ */
+int .main(void) 
+{
+puts ("\"Programming is like building a multilingual puzzle");
+return (0);
 }
