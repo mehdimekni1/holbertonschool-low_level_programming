c13fcebbc4563795799868286e526ae894d8730b
mehdimekni1 <7712@holbertonstudents.com>
2023-10-15 00:32:15 -0700

    sizeof

--- hello_world/6-size.c
+++ hello_world/6-size.c
@@ -14,11 +14,11 @@ int main(void)
         long long int llgintType;
 	long int longintType;
 	/* size of evaluates the size of a variable */
-printf("Size of a char: %zu bytes\n", sizeof(charType));
-printf("Size of an int: %zu bytes\n", sizeof(intType));
-printf("Size of a long int: %zu bytes\n", sizeof(longintType));
-printf("Size of a long long int: %zu bytes\n", sizeof(llgType));
-printf("Size of float: %zu bytes\n", sizeof(floatType));
+printf("Size of a char: %zu byte(s)\n", sizeof(charType));
+printf("Size of an int: %zu byte(s)\n", sizeof(intType));
+printf("Size of a long int: %zu byte(s)\n", sizeof(longintType));
+printf("Size of a long long int: %zu byte(s)\n", sizeof(llgType));
+printf("Size of float: %zu byte(s)\n", sizeof(floatType));
 	
 return (0);
 }
