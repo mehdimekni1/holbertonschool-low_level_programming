3b857d15b6f8216da1da5e9f89326602748d089c
mehdimekni1 <7712@holbertonstudents.com>
2023-10-15 00:39:55 -0700

    sizeof

--- hello_world/6-size.c
+++ hello_world/6-size.c
@@ -1,7 +1,7 @@
 #include <stdio.h>
 
 /**
- * main -  
+ * main -  "C program that prints the size of various types on the computer it is compliled and run on
  *
  *
  * Return: Always 0
@@ -14,11 +14,11 @@ int main(void)
         long long int llgintType;
 	long int longintType;
 	/* size of evaluates the size of a variable */
-printf("Size of a char: %zu byte(s)\n", sizeof(charType));
-printf("Size of an int: %zu byte(s)\n", sizeof(intType));
-printf("Size of a long int: %zu byte(s)\n", sizeof(longintType));
-printf("Size of a long long int: %zu byte(s)\n", sizeof(llgType));
-printf("Size of float: %zu byte(s)\n", sizeof(floatType));
+        printf("Size of a char: %zu byte(s)\n", sizeof(charType));
+        printf("Size of an int: %zu byte(s)\n", sizeof(intType));
+        printf("Size of a long int: %zu byte(s)\n", sizeof(longintType));
+        printf("Size of a long long int: %zu byte(s)\n", sizeof(llgType));
+        printf("Size of float: %zu byte(s)\n", sizeof(floatType));
 	
 return (0);
 }
