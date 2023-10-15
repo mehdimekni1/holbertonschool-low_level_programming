423b520073b5803804d6cd3b1d89d0e9f9e982e9
mehdimekni1 <7712@holbertonstudents.com>
2023-10-15 01:01:32 -0700

    sizeof

--- hello_world/6-size.c
+++ hello_world/6-size.c
@@ -17,7 +17,8 @@ int main(void)
         printf("Size of a char: %zu byte(s)\n", sizeof(charType));
         printf("Size of an int: %zu byte(s)\n", sizeof(intType));
         printf("Size of a long int: %zu byte(s)\n", sizeof(longintType));
-        printf("Size of a long long int: %zu byte(s)\n", sizeof(llgType));
+        printf("Size of a long long int: %zu byte(s)\n", sizeof(llgintType));
+	printf("Size of a float: %zu byte(s)\n", sizeof(floatType));
 
 	
 return (0);
