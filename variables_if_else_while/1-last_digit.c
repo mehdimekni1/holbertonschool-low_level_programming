84ed9ed56ed5c17ae632123fd231c024e8d19608
mehdimekni1 <7712@holbertonstudents.com>
2023-10-17 10:47:51 +0200

    declare y

--- variables_if_else_while/1-last_digit.c
+++ variables_if_else_while/1-last_digit.c
@@ -8,7 +8,7 @@
  */
 int main(void)
 {
-	int n;
+	int n, y;
 
 	srand(time(0));
 	n = rand() - RAND_MAX / 2;
@@ -21,7 +21,7 @@ int main(void)
 	{
         printf("Last digit of %d is %d and is 0\n", n, y);
     }
-    else (y < 6 )
+    else (y < 6)
     {
         printf("Last digit of %d is %d and is less than 6 and not 0\n", n, y);
     }
