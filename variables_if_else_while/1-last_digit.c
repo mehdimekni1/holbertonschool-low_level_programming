af91122843b672bbe51ef12c15220709e6174a73
mehdimekni1 <7712@holbertonstudents.com>
2023-10-17 10:54:23 +0200

    betty last error

--- variables_if_else_while/1-last_digit.c
+++ variables_if_else_while/1-last_digit.c
@@ -12,18 +12,18 @@ int main(void)
 
 	srand(time(0));
 	n = rand() - RAND_MAX / 2;
-    y = n % 10;
+	y = n % 10;
 	if (y > 5)
 	{
 		printf("Last digit of %d is %d and is greater than 5\n", n, y);
 	}
-    else if (y == 0)
+	else if (y == 0)
 	{
-        printf("Last digit of %d is %d and is 0\n", n, y);
-    }
-    else 
-    {
-        printf("Last digit of %d is %d and is less than 6 and not 0\n", n, y);
-    }
+		printf("Last digit of %d is %d and is 0\n", n, y);
+	}
+	else
+	{
+		printf("Last digit of %d is %d and is less than 6 and not 0\n", n, y);
+	}
 	return (0);
 }
