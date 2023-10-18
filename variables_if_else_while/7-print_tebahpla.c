3f23ba1688712bd2ccc0e5e4545452db520dbbfc
mehdimekni1 <7712@holbertonstudents.com>
2023-10-18 10:13:24 +0200

    reverse alphabet minus

--- variables_if_else_while/7-print_tebahpla.c
+++ variables_if_else_while/7-print_tebahpla.c
@@ -8,11 +8,11 @@ int main(void)
 {
 	char alphabet;
 
-	alphabet ='z';
+	alphabet = 'z';
 	while (alphabet <= 'a')
 	{
 		putchar(alphabet);
-		alphabet++;
+		alphabet--;
 	}
 	putchar('\n');
 	return (0);
