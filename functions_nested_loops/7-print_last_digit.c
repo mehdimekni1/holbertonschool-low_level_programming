2e17e809515faf90340af84cef2a33682f29fc38
mehdimekni1 <7712@holbertonstudents.com>
2023-10-19 14:14:51 +0200

    print last digit while

--- functions_nested_loops/7-print_last_digit.c
+++ functions_nested_loops/7-print_last_digit.c
@@ -6,7 +6,18 @@
  */
 int print_last_digit(int n)
 {
-n = n % 10;
-return (n);
-}
+    int x;
+    x = n % 10;
 
+    while (x >= 10)
+    {
+        x = x % 10;
+    }
+
+    if (x <= 0)
+    {
+        x = x * -1;
+    }
+    _putchar(x + '0');
+    return (x);
+}
