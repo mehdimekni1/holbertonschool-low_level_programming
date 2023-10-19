48dd23043bd6bf37921f29d62051d71af283eae4
mehdimekni1 <7712@holbertonstudents.com>
2023-10-19 14:16:30 +0200

    betty style

--- functions_nested_loops/7-print_last_digit.c
+++ functions_nested_loops/7-print_last_digit.c
@@ -6,18 +6,17 @@
  */
 int print_last_digit(int n)
 {
-    int x;
-    x = n % 10;
+int x;
 
-    while (x >= 10)
-    {
-        x = x % 10;
-    }
-
-    if (x <= 0)
-    {
-        x = x * -1;
-    }
-    _putchar(x + '0');
-    return (x);
+ x = n % 10;
+while (x >= 10)
+{
+x = x % 10;
+}
+if (x <= 0)
+{
+x = x * -1;
+}
+_putchar(x + '0');
+return (x);
 }
