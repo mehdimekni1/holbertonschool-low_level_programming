5eb63c622e6fba240a7771931cbffb74d9c19090
mehdimekni1 <7712@holbertonstudents.com>
2023-10-23 09:56:49 +0200

    add test

---
+++ functions_nested_loops/10-add.c
@@ -0,0 +1,14 @@
+#include "main.h"
+/**
+ * add- a function that adds two integers and returns the result.
+ *
+ * Return:0
+ */
+int add(int, int)
+{
+    int a, b, c;  
+    
+    a = b + c 
+    printf("%d + %d = %d, b, c, a")
+    return (0);
+}
