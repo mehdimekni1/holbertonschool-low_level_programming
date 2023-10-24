eea819b5d03d5086f98c05772c7fffd4615e30d7
mehdimekni1 <7712@holbertonstudents.com>
2023-10-24 10:45:31 +0200

    it works as expected

--- functions_nested_loops/11-print_to_98.c
+++ functions_nested_loops/11-print_to_98.c
@@ -10,11 +10,13 @@ void print_to_98(int n)
 {
     for (n = n; n < 99; n++)
     {
-        printf("%d, ", n);
         if (n == 98)
         {
-            printf("%d\n", n);
+            printf("%d", n);
         }
+        else 
+        printf("%d, ", n);
+       
     }
     printf("\n");
 }
