c8d43550ae3fd3041d16c46a634720a3310cc879
mehdimekni1 <7712@holbertonstudents.com>
2023-10-24 10:22:52 +0200

    print all n to 98 test

--- functions_nested_loops/11-print_to_98.c
+++ functions_nested_loops/11-print_to_98.c
@@ -11,6 +11,10 @@ void print_to_98(int n)
     for (n = n; n < 99; n++)
     {
         printf("%d, ", n);
+        if (n == 98)
+        {
+            printf("%d\n", n);
+        }
     }
     printf("\n");
 }
